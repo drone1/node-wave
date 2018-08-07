0b7d7767f5c278550803901bc6fac83734fbd05f
Jon Lippincott <jonlippincott@gmail.com>
2018-08-07 00:33:54 -0400

    Added support for getting at the normalized bar data. Useful for those of us who don't want to use images, but do want the data to render to canvas, etc.

--- test/render.js
+++ test/render.js
@@ -5,3 +5,21 @@ render('Canon.mp3', function (err, buffer) {
   if (err) return err
   fs.writeFileSync('out.png', buffer)
 })
+
+var height = 256;
+render('Canon.mp3', {
+		barWidth: 1,
+		baselineWidth: 0,
+		baseline: height / 2,
+		padding: 2,
+		width: height * 4,
+		height: height,
+
+		normalizedValuesCallback: function(normalizedVals) {
+			console.log(normalizedVals)
+		}
+	},
+	function(err, buffer) {
+	  if (err) return err
+	}
+)
