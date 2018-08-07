fd4a3b99e5ed8ae929973a04fafb9bd2daee159c
Jon Lippincott <jonlippincott@gmail.com>
2018-08-07 01:05:16 -0400

    Because why not, I made the test render the waveform.

--- test/render.js
+++ test/render.js
@@ -1,6 +1,14 @@
 var render = require('../lib/render')
 var fs = require('fs')
 
+function msg(s) {
+	process.stdout.write(s || '');
+}
+
+function msgnl(s) {
+	msg(`${(s || '')}\n`);
+}
+
 render('Canon.mp3', function (err, buffer) {
   if (err) return err
   fs.writeFileSync('out.png', buffer)
@@ -16,7 +24,17 @@ render('Canon.mp3', {
 		height: height,
 
 		normalizedValuesCallback: function(normalizedVals) {
-			console.log(normalizedVals)
+			const numLines = 20;
+			const numSamples = Math.min(process.stdout.columns, normalizedVals.length);
+
+			msgnl();
+			for (let line = numLines; line >= 0; --line) {
+				for (let idx = 0; idx < numSamples; ++idx) {
+					const val = normalizedVals[idx];
+					msg(val >= line/(numLines - 1) ? '|' : ' ');
+				}
+				msgnl();
+			}
 		}
 	},
 	function(err, buffer) {
